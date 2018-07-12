65b03d25bcc63b8284b014149c8eeecec6fd1c07
Santiago Hernandez <santiago.hdz.b@gmail.com>
2018-07-11 19:55:40 -0500

    working on windows with more or less portable python functions

--- train.py
+++ train.py
@@ -1,91 +1,114 @@
-#!/usr/bin/python
+#!\\usr\\bin\\python
 
+from os import path
+from os.path import join
 import os
 import glob
 import sys
 
-
-TESSERACT_DIR='/storage/projects/alpr/libraries/tesseract-ocr'
+# country = raw_input("Two-Letter Country Code to Train: ").lower()
+country = "eu"
+CURRENT_DIR = os.getcwd()
+TESSERACT_DIR = 'G:\\AB\\TCGAS\\DEMOS\\train-ocr\\tesseract'
+TESSERACT_BIN = join(TESSERACT_DIR, "tesseract")
+TEMP_DIR = join(CURRENT_DIR, "tmp")
+TESSERACT_TRAINDIR = TESSERACT_DIR
+LANGUAGE_NAME = 'l' + country
 
 os.environ["TESSDATA_PREFIX"] = TESSERACT_DIR
-#os.system("export TESSDATA_PREFIX=" + TESSERACT_DIR)
+box_files = glob.glob(join(CURRENT_DIR, country, "input", "") + "*.box")
 
-TESSERACT_BIN=TESSERACT_DIR + '/tesseract'
-TESSERACT_TRAINDIR= TESSERACT_DIR + '/training'
-
-
-country = raw_input("Two-Letter Country Code to Train: ").lower()
-
-LANGUAGE_NAME='l' + country
-
-box_files = glob.glob('./' + country + '/input/*.box')
 if not box_files:
-    print "Cannot find input files"
+    print("Cannot find input files")
     sys.exit(1)
+    
+os.system('del /Q "%s"' % join(TEMP_DIR, "*"))
+font_properties_file = open(join(TEMP_DIR, "font_properties"),'w')
 
-os.system("rm ./tmp/*")
-
-font_properties_file = open('./tmp/font_properties','w')
+BOX_FILES_LIST = ""
+TR_FILES_LIST = ""
 
 for box_file in box_files:
-    print "Processing: " + box_file
+    print("Processing: " + box_file)
 
-    file_without_dir = os.path.split(box_file)[1]
-    file_without_ext = os.path.splitext(file_without_dir)[0]
-    input_dir = os.path.dirname(box_file)
+    file_without_dir = path.split(box_file)[1]
+    file_without_ext = path.splitext(file_without_dir)[0]
+    input_dir = path.dirname(box_file)
+    tif_file = join(input_dir, file_without_ext + ".tif")
+    train_cmd = '%s %s %s box.train.stderr' % (TESSERACT_BIN, tif_file, file_without_ext)
 
-    tif_file = input_dir + '/' + file_without_ext + ".tif"
-
-    train_cmd = "%s -l eng %s %s nobatch box.train.stderr" % (TESSERACT_BIN, tif_file, file_without_ext)
-    print "Executing: " + train_cmd 
+    print("Executing: " + train_cmd)
     os.system(train_cmd)
-    os.system("mv ./" + file_without_ext + ".tr ./tmp/" + file_without_ext + ".tr")
-    os.system("mv ./" + file_without_ext + ".txt ./tmp/" + file_without_ext + ".txt")
 
-    font_name=file_without_dir.split('.')[1]
+    output_tr = file_without_ext + ".tr"
+    output_txt = file_without_ext + ".txt"
+
+    if (path.exists(join(CURRENT_DIR, output_tr))):
+        os.system("move %s %s" % (join(CURRENT_DIR, output_tr), join(TEMP_DIR, output_tr)))
+    else:
+        print("Train did not work as there is not output files")
+        sys.exit(1)
+
+    if (path.exists(join(CURRENT_DIR, output_txt))):
+        os.system("move %s %s" % (join(CURRENT_DIR, output_txt), join(TEMP_DIR, output_txt)))
+
+    font_name = file_without_dir.split('.')[1]
     font_properties_file.write(font_name + ' 0 0 1 0 0\n')
+
+    BOX_FILES_LIST += '"' + box_file + '" '
+    TR_FILES_LIST += '"' + join(TEMP_DIR, output_tr) + '" '
 
 font_properties_file.close()
 
-os.system(TESSERACT_TRAINDIR + "/unicharset_extractor ./" + country + "/input/*.box")
-#os.system('mv ./unicharset ./" + country + "/input/" + LANGUAGE_NAME + ".unicharset')
-
-# Shape clustering should currently only be used for the "indic" languages
-#train_cmd = TESSERACT_TRAINDIR + '/shapeclustering -F ./' + country + '/input/font_properties -U unicharset ./' + country + '/input/*.tr'
-#print "Executing: " + train_cmd
-#os.system(train_cmd)
-
-
-train_cmd = TESSERACT_TRAINDIR + '/mftraining -F ./tmp/font_properties -U unicharset -O ./tmp/' + LANGUAGE_NAME + '.unicharset ./tmp/*.tr'
-print "Executing: " + train_cmd
+train_cmd = join(TESSERACT_TRAINDIR, "unicharset_extractor") + " " + BOX_FILES_LIST
+print("Executing: " + train_cmd)
 os.system(train_cmd)
-os.system("rm ./unicharset")
-os.system("mv ./tmp/" + LANGUAGE_NAME + ".unicharset ./")
-os.system("cp ./" + country + "/input/unicharambigs ./" + LANGUAGE_NAME + ".unicharambigs")
+os.system("move %s %s" % (join(CURRENT_DIR, "unicharset"), join(TEMP_DIR, LANGUAGE_NAME + ".unicharset")))
 
+train_cmd = '%s -F "%s" -U unicharset -O "%s" %s' % (join(TESSERACT_TRAINDIR, "mftraining"), join(TEMP_DIR, "font_properties"), join(TEMP_DIR, LANGUAGE_NAME + ".unicharset"), TR_FILES_LIST)
+print("Executing: " + train_cmd)
+os.system(train_cmd)
 
-os.system(TESSERACT_TRAINDIR + '/cntraining ./tmp/*.tr')
+if (path.exists(join(CURRENT_DIR, "unicharset"))):
+    os.system("del " + join(CURRENT_DIR, "unicharset"))
+if (path.exists(join(TEMP_DIR, LANGUAGE_NAME + ".unicharset"))):
+    os.system("move " + join(TEMP_DIR, LANGUAGE_NAME + ".unicharset") + " " + CURRENT_DIR)
+if (path.exists(join(CURRENT_DIR, country, "input", "unicharambigs"))):
+    os.system("copy " + join(CURRENT_DIR, country, "input", "unicharambigs") + path.join(CURRENT_DIR, LANGUAGE_NAME + ".unicharambigs"))
 
-#os.system("mv ./unicharset ./" + LANGUAGE_NAME + ".unicharset")
-os.system("mv ./shapetable ./" + LANGUAGE_NAME + ".shapetable")
-#os.system("rm ./shapetable")
-os.system("mv ./pffmtable ./" + LANGUAGE_NAME + ".pffmtable")
-os.system("mv ./inttemp ./" + LANGUAGE_NAME + ".inttemp")
-os.system("mv ./normproto ./" + LANGUAGE_NAME + ".normproto")
+train_cmd = join(TESSERACT_TRAINDIR, "cntraining") + ' ' + TR_FILES_LIST
+print("Executing: " + train_cmd)
+os.system(train_cmd)
 
+if (path.exists(join(CURRENT_DIR, "shapetable"))):
+    os.system("move %s %s" % (join(CURRENT_DIR, "shapetable"), join(CURRENT_DIR, LANGUAGE_NAME + ".shapetable")))
+if (path.exists(join(CURRENT_DIR, "pffmtable"))):
+    os.system("move %s %s" % (join(CURRENT_DIR, "pffmtable"), join(CURRENT_DIR, LANGUAGE_NAME + ".pffmtable")))
+if (path.exists(join(CURRENT_DIR, "inttemp"))):
+    os.system("move %s %s" % (join(CURRENT_DIR, "inttemp"), join(CURRENT_DIR, LANGUAGE_NAME + ".inttemp")))
+if (path.exists(join(CURRENT_DIR, "normproto"))):
+    os.system("move %s %s" % (join(CURRENT_DIR, "normproto"), join(CURRENT_DIR, LANGUAGE_NAME + ".normproto")))
 
-os.system(TESSERACT_TRAINDIR + '/combine_tessdata ' + LANGUAGE_NAME + '.')
+train_cmd = join(TESSERACT_TRAINDIR, "combine_tessdata") + " " + LANGUAGE_NAME + "."
+print("Executing: " + train_cmd)
+os.system(train_cmd)
 
 # If a config file is in the country's directory, use that.
-config_file = os.path.join('./', country, country + '.config')
-if os.path.isfile(config_file):
-    print "Applying config file: " + config_file
+config_file = join(CURRENT_DIR, country, country + '.config')
+if path.isfile(config_file):
+    print("Applying config file: " + config_file)
     trainedata_file = LANGUAGE_NAME + '.traineddata'
-    os.system(TESSERACT_TRAINDIR + '/combine_tessdata -o ' + trainedata_file + ' ' + config_file )
+    os.system(join(TESSERACT_TRAINDIR, "combine_tessdata") + ' -o ' + trainedata_file + ' ' + config_file)
 
-os.system("mv ./" + LANGUAGE_NAME + ".unicharset ./tmp/")
-os.system("mv ./" + LANGUAGE_NAME + ".shapetable ./tmp/")
-os.system("mv ./" + LANGUAGE_NAME + ".pffmtable ./tmp/")
-os.system("mv ./" + LANGUAGE_NAME + ".inttemp ./tmp/")
-os.system("mv ./" + LANGUAGE_NAME + ".normproto ./tmp/")
-os.system("mv ./" + LANGUAGE_NAME + ".unicharambigs ./tmp/")
+if (path.exists(join(CURRENT_DIR, LANGUAGE_NAME + ".unicharset"))):
+    os.system("move %s %s" % (join(CURRENT_DIR, LANGUAGE_NAME + ".unicharset"), TEMP_DIR))
+if (path.exists(join(CURRENT_DIR, LANGUAGE_NAME + ".shapetable"))):
+    os.system("move %s %s" % (join(CURRENT_DIR, LANGUAGE_NAME + ".shapetable"), TEMP_DIR))
+if (path.exists(join(CURRENT_DIR, LANGUAGE_NAME + ".pffmtable"))):
+    os.system("move %s %s" % (join(CURRENT_DIR, LANGUAGE_NAME + ".pffmtable"), TEMP_DIR))
+if (path.exists(join(CURRENT_DIR, LANGUAGE_NAME + ".inttemp"))):
+    os.system("move %s %s" % (join(CURRENT_DIR, LANGUAGE_NAME + ".inttemp"), TEMP_DIR))
+if (path.exists(join(CURRENT_DIR, LANGUAGE_NAME + ".normproto"))):
+    os.system("move %s %s" % (join(CURRENT_DIR, LANGUAGE_NAME + ".normproto"), TEMP_DIR))
+if (path.exists(join(CURRENT_DIR, LANGUAGE_NAME + ".unicharambigs"))):
+    os.system("move %s %s" % (join(CURRENT_DIR, LANGUAGE_NAME + ".unicharambigs"), TEMP_DIR))
